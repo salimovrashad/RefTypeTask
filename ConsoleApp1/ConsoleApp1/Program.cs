2e22ad668469fb2dc499b39e16fdc42a533fe466
salimovrashad <selimovresad33@gmail.com>
2023-10-19 02:16:21 +0400

    add task file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 8, -11, 16, -4, 7, -8, 9, 22 };
+            Negative(arr);
+        }
+        static void Negative(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    arr[i] *= -1;
+                }
+                Console.WriteLine(arr[i]);
+            }
+        }
+        
+    }
+}
